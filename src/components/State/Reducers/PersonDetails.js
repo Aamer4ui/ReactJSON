90601924e9a977600375b00c59bbb90e98089646
Aamer4ui <aamersohelansari@gmail.com>
2021-02-13 16:17:23 +0530

    details

--- src/components/State/Reducers/PersonDetails.js
+++ src/components/State/Reducers/PersonDetails.js
@@ -1,2 +1,10 @@
-const PersonDetails =()=>{
+const PersonDetails = () => {
+    person:[
+        {id:"12fgs", name:"Aamer" ,title:"Full Stack Devloper"}
+        {id:"14kgf", name:"Zubair" ,title:"Python Devloper"}
+        {id:"10bgd", name:"Sameer" ,title:"Full Stack Devloper"}
+        {id:"9gf6", name:"Majeed" ,title:"Full Stack Devloper"}
+        {id:"64tqs", name:"Ashmer" ,title:"Full Stack Devloper"}
+        {id:"89pmn", name:"Salman" ,title:"Full Stack Devloper"}
+    ]
 }
