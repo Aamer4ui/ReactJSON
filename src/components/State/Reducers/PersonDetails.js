357abf088f41667006b2f32ef11d12d10bb12adc
Aamer4ui <aamersohelansari@gmail.com>
2021-02-13 16:36:37 +0530

    some small changes for error solving

--- src/components/State/Reducers/PersonDetails.js
+++ src/components/State/Reducers/PersonDetails.js
@@ -6,5 +6,5 @@ const PersonDetails = () => {
         {id:"9gf6", name:"Majeed" ,age:"24", title:"Full Stack Devloper"},
         {id:"64tqs", name:"Ashmer" ,age:"26 ",title:"Full Stack Devloper"},
         {id:"89pmn", name:"Salman" ,age:"27",title:"Full Stack Devloper"},
-    ],
+    ]
 }
