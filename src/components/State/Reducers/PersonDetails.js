17683072640dac71cf980ab3ed51f5d7bf051415
Aamer4ui <aamersohelansari@gmail.com>
2021-02-13 16:52:43 +0530

    adding information

--- src/components/State/Reducers/PersonDetails.js
+++ src/components/State/Reducers/PersonDetails.js
@@ -2,9 +2,9 @@ const PersonDetails = () => {
     person:[
         {id:"12fgs", name:"Aamer" , age:"24" , title:"Full Stack Devloper"},
         {id:"14kgf", name:"Zubair" , age:"21" , title:"Python Devloper"},
-        {id:"10bgd", name:"Sameer" , age:"25", title:"Full Stack Devloper"},
-        {id:"9gf6", name:"Majeed" ,age:"24", title:"Full Stack Devloper"},
-        {id:"64tqs", name:"Ashmer" ,age:"26 ",title:"Full Stack Devloper"},
-        {id:"89pmn", name:"Salman" ,age:"27",title:"Full Stack Devloper"},
+        {id:"10bgd", name:"Sameer" , age:"25", title:"Java Stack Devloper"},
+        {id:"9gf6", name:"Majeed" ,age:"24", title:"Hardware Engineer"},
+        {id:"64tqs", name:"Ashmer" ,age:"26 ",title:"Marketing"},
+        {id:"89pmn", name:"Salman" ,age:"27",title:"Dababase"},
     ]
 }
