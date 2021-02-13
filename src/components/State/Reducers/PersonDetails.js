5c1cb1b536c09aa4586c92e4707d8f66e58fda7a
Aamer4ui <aamersohelansari@gmail.com>
2021-02-13 16:23:33 +0530

    adding age

--- src/components/State/Reducers/PersonDetails.js
+++ src/components/State/Reducers/PersonDetails.js
@@ -1,7 +1,7 @@
 const PersonDetails = () => {
     person:[
-        {id:"12fgs", name:"Aamer" ,title:"Full Stack Devloper"},
-        {id:"14kgf", name:"Zubair" ,title:"Python Devloper"},
+        {id:"12fgs", name:"Aamer" , age:"24" ,title:"Full Stack Devloper"},
+        {id:"14kgf", name:"Zubair" ,age title:"Python Devloper"},
         {id:"10bgd", name:"Sameer" ,title:"Full Stack Devloper"},
         {id:"9gf6", name:"Majeed" ,title:"Full Stack Devloper"},
         {id:"64tqs", name:"Ashmer" ,title:"Full Stack Devloper"},
