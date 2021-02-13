dd713a540eeee70a412363e80fd66c1ba25b1210
Aamer4ui <aamersohelansari@gmail.com>
2021-02-13 16:25:47 +0530

    hello

--- src/components/State/Reducers/PersonDetails.js
+++ src/components/State/Reducers/PersonDetails.js
@@ -3,8 +3,8 @@ const PersonDetails = () => {
         {id:"12fgs", name:"Aamer" , age:"24" , title:"Full Stack Devloper"},
         {id:"14kgf", name:"Zubair" , age:"21" , title:"Python Devloper"},
         {id:"10bgd", name:"Sameer" , age:"25", title:"Full Stack Devloper"},
-        {id:"9gf6", name:"Majeed" ,title:"Full Stack Devloper"},
-        {id:"64tqs", name:"Ashmer" ,title:"Full Stack Devloper"},
+        {id:"9gf6", name:"Majeed" ,age:"24", title:"Full Stack Devloper"},
+        {id:"64tqs", name:"Ashmer" ,age:"26 ",title:"Full Stack Devloper"},
         {id:"89pmn", name:"Salman" ,title:"Full Stack Devloper"},
     ],
 }
