aef799faedf241024cbc05ec89af108a3db8c26b
Aamer4ui <aamersohelansari@gmail.com>
2021-02-15 17:44:22 +0530

    card added

--- src/components/MainBody/FetchingDataParent.jsx
+++ src/components/MainBody/FetchingDataParent.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import { PersonalDetail, PersonDetails } from "../State/Reducers/PersonDetails";
+import FetchingDataChild from "./FetchingDataChild";
 
 export const FetchingDataParent = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  return <div>
+      {PersonDetails.pesron.map((employee)=>{
+          return <FetchingDataChild employeeInfo={employee}/>
+      })
 
+      }</div>;
+};
