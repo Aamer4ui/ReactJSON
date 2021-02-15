08a90c1be64670b2ecdc1dac73e90669b1ce1d4d
Aamer4ui <aamersohelansari@gmail.com>
2021-02-15 16:37:24 +0530

    second image

--- src/components/State/Reducers/PersonDetails.js
+++ src/components/State/Reducers/PersonDetails.js
@@ -2,7 +2,7 @@ export const PersonDetails = {
     person:[
         {id:"12fgs", name:"Aamer" , age:"24" , title:"Full Stack Devloper", image:"https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg"},
         {id:"14kgf", name:"Zubair" , age:"21" , title:"Python Devloper", image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"},
-        {id:"10bgd", name:"Sameer" , age:"25", title:"Java Stack Devloper"},
+        {id:"10bgd", name:"Sameer" , age:"25", title:"Java Stack Devloper",image:"https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg"},
         {id:"9gf6", name:"Majeed" ,age:"24", title:"Hardware Engineer"},
         {id:"64tqs", name:"Ashmer" ,age:"26 ",title:"Marketing"},
         {id:"89pmn", name:"Salman" ,age:"27",title:"Dababase"},
