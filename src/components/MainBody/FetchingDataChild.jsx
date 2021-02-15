daa7b3b98adba681974fa33bd3080cd974f6380b
Aamer4ui <aamersohelansari@gmail.com>
2021-02-15 19:46:13 +0530

    card content

--- src/components/MainBody/FetchingDataChild.jsx
+++ src/components/MainBody/FetchingDataChild.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import "../MainBody/Fetching.css";
 
 const FetchingDataChild = ({ Info }) => {
   return (
-    <div>
+    <div className="card-body">
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={Info.image} />
         <Card.Body>
