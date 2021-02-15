637a840ca02f942e5c8dbad570ad49e6700ae91a
Aamer4ui <aamersohelansari@gmail.com>
2021-02-15 16:38:56 +0530

    image 4

--- src/components/State/Reducers/PersonDetails.js
+++ src/components/State/Reducers/PersonDetails.js
@@ -4,7 +4,7 @@ export const PersonDetails = {
         {id:"14kgf", name:"Zubair" , age:"21" , title:"Python Devloper", image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"},
         {id:"10bgd", name:"Sameer" , age:"25", title:"Java Stack Devloper",image:"https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg"},
         {id:"9gf6", name:"Majeed" ,age:"24", title:"Hardware Engineer", image:"https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
-        {id:"64tqs", name:"Ashmer" ,age:"26 ",title:"Marketing"},
+        {id:"64tqs", name:"Ashmer" ,age:"26 ",title:"Marketing", image:"https://i.guim.co.uk/img/media/b68ee32677173fe677fc8a8d408634e721126f83/850_401_2104_1262/master/2104.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=d5535fa131c552b9fa4c523302e8686e"},
         {id:"89pmn", name:"Salman" ,age:"27",title:"Dababase"},
     ]
 }
